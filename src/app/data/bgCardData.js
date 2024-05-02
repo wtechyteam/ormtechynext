1e29aab5dd978a239c3fdb02f29fe1d88f188eac
HP <you@example.com>
2024-05-02 18:29:59 +0530

    mobile-menu and bgcard

--- src/app/data/bgCardData.js
+++ src/app/data/bgCardData.js
@@ -89,4 +89,5 @@ export const bgCardData = [
         textContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. ",
         rating: "/images/star-rating.svg"
     },
+    
 ]
