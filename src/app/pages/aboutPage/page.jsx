9fb5e19bba9874993f5a9c219f97a6772aee111d
HP <you@example.com>
2024-05-06 18:51:27 +0530

    responsive

--- src/app/pages/aboutPage/page.jsx
+++ src/app/pages/aboutPage/page.jsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 import PrimeBtn from "../common/primeBtn"
 import Marquees from "../marquee"
 import { marqueeCardData } from "@/app/data/marqueeData"
+import { Col, Container, Row } from "react-bootstrap"
 
 const AboutPage = () => {
     return (
@@ -45,18 +46,24 @@ const AboutPage = () => {
             <section className=' bg_dark position-relative'>
                 <div className=' image_bg'>
                     <div className='content_bg sectionPadding2'>
-                        <div className='item_center mb-5 '>
-                            <Image width={40} height={40} src={'/images/Google-logo.png'} alt='img' title='rating logo' />
-                            <h2 className='text-white mx-3 mb-0 text-24 fw-bold'>5.0</h2>
-                            <div className='d-flex gap-2'>
-                                <Image width={22} height={22} src={"/images/star-rating.svg"} alt='star' title='start rating' />
-                                <Image width={22} height={22} src={"/images/star-rating.svg"} alt='star' title='start rating' />
-                                <Image width={22} height={22} src={"/images/star-rating.svg"} alt='star' title='start rating' />
-                                <Image width={22} height={22} src={"/images/star-rating.svg"} alt='star' title='start rating' />
-                                <Image width={22} height={22} src={"/images/star-rating.svg"} alt='star' title='start rating' />
-                            </div>
-                            <div className='divider'></div>
-                            <h2 className='text-white mb-0 text-24'>Based on 240+ reviews</h2>
+                        <div className='item_center mb-5 container'>
+                            <Row>
+                                <Col md={6} className="item_center flex-wrap mb-md-0 mb-2">
+                                    <Image width={40} height={40} src={'/images/Google-logo.png'} alt='img' title='rating logo' />
+                                    <h2 className='text-white mx-3 mb-0 text-24 fw-bold'>5.0</h2>
+                                    <div className='d-flex gap-2'>
+                                        <Image width={22} height={22} src={"/images/star-rating.svg"} alt='star' title='start rating' />
+                                        <Image width={22} height={22} src={"/images/star-rating.svg"} alt='star' title='start rating' />
+                                        <Image width={22} height={22} src={"/images/star-rating.svg"} alt='star' title='start rating' />
+                                        <Image width={22} height={22} src={"/images/star-rating.svg"} alt='star' title='start rating' />
+                                        <Image width={22} height={22} src={"/images/star-rating.svg"} alt='star' title='start rating' />
+                                    </div>
+                                </Col>
+                                <Col md={6} className="item_center position-relative">
+                                    <div className='divider'></div>
+                                    <h2 className='text-white mb-0 text-24 no_wrap ms-0 ms-md-2'>Based on 240+ reviews</h2>
+                                </Col>
+                            </Row>
                         </div>
                         <Marquees mSpeed={60} pauseOnHover={true} mapData={marqueeCardData} dataType="card" />
                     </div>
