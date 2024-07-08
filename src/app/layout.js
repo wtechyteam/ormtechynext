ba6c53c55ac8e35fc2515afe21ae0b841ec4a3f3
WTechy <wtechy@gmail.com>
2024-07-08 17:45:42 +0530

    gsc/ga

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,31 +1,43 @@
-
+/* eslint-disable @next/next/next-script-for-ga */
+// root-layout.jsx
 import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./../../public/assets/style/customStyle.scss"
+import "./../../public/assets/style/customStyle.scss";
 import HeaderNavbar from "./header/header";
 import Footer from "./footer/footer";
 
-import { metadata } from "./data/metaData";
-
-
 export default function RootLayout({ children }) {
-
-
   return (
     <html>
-      <body>
+      <head>
+        {/* Google Site Verification */}
+        <meta name="google-site-verification" content="WRnUipEdUORvYwjBMoasRBifV44oaePJxnUK-WFmies" />
         
-           <HeaderNavbar />
-          {children}
-           <Footer />
-
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3Z6RYNC4LT"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-3Z6RYNC4LT');
+            `,
+          }}
+        />
+      </head>
+      <body>
+        <HeaderNavbar />
+        {children}
+        <Footer />
       </body>
     </html>
   );
 }
-export function generateMetadata(){
-  return{
-    title:"Online Reputation Management Services | ORM Techies",
-    description:"Trusted Online Reputation Management Services by ORM Techies. Specializing in personal branding, reputation repair, and proactive management. Contact us now!"
+
+export function generateMetadata() {
+  return {
+    title: "Online Reputation Management Services | ORM Techies",
+    description: "Trusted Online Reputation Management Services by ORM Techies. Specializing in personal branding, reputation repair, and proactive management. Contact us now!"
   }
 }
