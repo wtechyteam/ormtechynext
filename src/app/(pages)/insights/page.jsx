4260897a6cd5713f5ac3f53cf6a1bdabc6c9510a
WTechy <wtechy@gmail.com>
2024-07-15 16:00:45 +0530

    updaate image

--- src/app/(pages)/insights/page.jsx
+++ src/app/(pages)/insights/page.jsx
@@ -49,6 +49,7 @@ const OrmTechiesInMediaPage = () => {
       <InnerTopBanner
         title="ORMTechies Insights"
         info="Are you looking to boost your online presence and reputation? At ORMTechies, we specialize in advanced online review management that can significantly enhance your business's image."
+        imageSrc="./images/contact.png"
       />
       <Container>
         <div className="my-4 text-center">
