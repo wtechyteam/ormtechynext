75e0185878e0ee23dd5cfb64a8e380377a84b1bc
WTechy <wtechy@gmail.com>
2024-07-17 12:25:46 +0530

    new commit

--- src/app/(pages)/insights/[slug]/page.jsx
+++ src/app/(pages)/insights/[slug]/page.jsx
@@ -1,10 +1,10 @@
-/* eslint-disable @next/next/no-img-element */
-import { client,urlFor} from "../../../../utils/configSanity";
-import React from 'react';
-import { PortableText } from '@portabletext/react';
-import InnerTopBanner from "../../common/innerTopBanner";
+// /* eslint-disable @next/next/no-img-element */
+// import { client,urlFor} from "../../../../utils/configSanity";
+// import React from 'react';
+// import { PortableText } from '@portabletext/react';
+// import InnerTopBanner from "../../common/innerTopBanner";
 
-async function getData(slug) {
+/* async function getData(slug) {
   const query = `*[_type=="insights" && slug.current==$slug]{
     title, shortDescription, image, body
   } [0]`;
@@ -20,30 +20,30 @@ export async function generateStaticParams() {
     slug: post.slug,
   }));
 }
+ */
+// export default async function BlogPost({ params }) {
+  /* const post = await getData(params.slug); */
 
-export default async function BlogPost({ params }) {
-  const post = await getData(params.slug);
-
-  return (
+/*   return (
     <>
       <InnerTopBanner
         title={post.title}
         info={post.shortDescription}
         imageSrc="./images/contact.png"
       />
-      <div style={styles.container}>
+     {/*  <div style={styles.container}>
         <h1 style={styles.title}>{post.title}</h1>
          <img src={urlFor(post.image).url()} alt={post.title} style={styles.image} /> 
         <p style={styles.description}>{post.shortDescription}</p>
         <div style={styles.body}>
           <PortableText value={post.body} />
         </div>
-      </div>
+      </div> 
     </>
   );
-}
+} 
 
-const styles = {
+/* const styles = {
   container: {
     display: 'flex',
     flexDirection: 'column',
@@ -67,3 +67,4 @@ const styles = {
     width: '100%',
   },
 };
+ */
