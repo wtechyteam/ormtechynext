17f4eb1c04285ebade53871c403f0f83d2c9b329
WTechy <wtechy@gmail.com>
2024-07-24 13:23:24 +0530

    footer updated

--- src/app/footer/footerBottom.jsx
+++ src/app/footer/footerBottom.jsx
@@ -11,8 +11,8 @@ const FooterBottom = () => {
       <div className="container">
         <div className="content-info">
           <div className="links">
-            <a className="privacy-policy-link" href="https://removify.com/terms-of-business/" previewlistener="true">Terms of Business</a>
-            <a href="https://removify.com/privacy-policy/" previewlistener="true">Privacy Policy</a>
+            <a className="privacy-policy-link" href="" previewlistener="true">Terms of Business</a>
+            <a href="" previewlistener="true">Privacy Policy</a>
           </div>
           <span className="copyright">Copyright © 2024 ORM Techies. All rights reserved.</span>
         </div>
