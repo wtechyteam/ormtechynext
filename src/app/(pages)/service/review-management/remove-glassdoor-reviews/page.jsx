786096c7ec608aa60b4d39e7d7d1257dbe2c0fe7
WTechy <wtechy@gmail.com>
2024-08-01 16:41:50 +0530

    glassdoor page updated

--- src/app/(pages)/service/review-management/remove-glassdoor-reviews/page.jsx
+++ src/app/(pages)/service/review-management/remove-glassdoor-reviews/page.jsx
@@ -15,19 +15,46 @@ const Glassdoor = () => {
          <meta name="robots" content="index, follow" />
        </head>
        <InnerTopBanner
-         title="From Reviews to Results: Get Insightful Glassdoor Reviews  "
-         info="They provide a window into your company's workplace culture and employee satisfaction, making them an invaluable tool for attracting top talent and maintaining a positive reputation.  "
+         title="Insightful Glassdoor Reviews  "
+         info="Glassdoor is a platform where current and former employees can anonymously review companies, share salary information, and provide insights about the work environment. ORM Techies helps businesses manage their Glassdoor presence by monitoring reviews, responding professionally to feedback, and implementing strategies to improve employee satisfaction and attract top talent.  "
        />
        <section className="sectionPadding bg_gray">
          <ImageInfoWrap
-           title={"Repair Your Glassdoor Image "}
-           imageUrl={"/images/maintenance-concept-illustration.png"}
+           title={"From Reviews to Results: Get Insightful Glassdoor Reviews"}
+           imageUrl={"/images/image06.png"}
            showPrimeBtn={true}
            primeBtnName={"get a free quote"}
            borderBtnName={"faq"}
            darkButton="dark_btn"
            description={
-             "<p>Online reviews have seeped into the workplace, reaching beyond products and services. Employees can share their thoughts and feelings about their jobs on Glassdoor. The culture, management, and overall environment are all reflected in these reviews. Positive reviews can enhance a company's reputation, but negative reviews can have significant adverse effects. </p> <p>  Positive and negative reviews harm a company's reputation, employee morale, and recruitment efforts. False information and defamation are some of the criteria for review removal in the guide. The success of ORMTechies in removing a damaging review demonstrates their expertise in review management. Best practices are provided for responding to and leveraging reviews to drive positive change. Best practices are provided for leveraging reviews to drive positive change.   </p>"
+             "<p>They provide a window into your company's workplace culture and employee satisfaction, making them an invaluable tool for attracting top talent and maintaining a positive reputation. </p><p>Glassdoor is often used by prospective employees to gain insight into a company's culture, management style, and overall employee satisfaction. Before applying for a job, the job seekers are likely to research company reviews and ratings before applying. Glassdoor reviews are a critical component of your company's online presence because of this. A positive online reputation is important. Your company's image remains untarnished with our comprehensive approach to managing reviews. </p>"
+           }
+         />
+       </section>
+       <section className="sectionPadding bg_white">
+         <ImageInfoWrap
+           title={"Repair Your Glassdoor Image "}
+           imageUrl={"/images/maintenance-concept-illustration.png"}
+           showPrimeBtn={true}
+           isReverse={true}
+           primeBtnName={"get a free quote"}
+           borderBtnName={"faq"}
+           darkButton="dark_btn"
+           description={
+             "<p>Online reviews have seeped into the workplace, reaching beyond products and services. Employees can share their thoughts and feelings about their jobs on Glassdoor. The culture, management, and overall environment are all reflected in these reviews. Positive reviews can enhance a company's reputation, but negative reviews can have significant adverse effects. </p> <p>  Positive and negative reviews harm a company's reputation, employee morale, and recruitment efforts. False information and defamation are some of the criteria for review removal in the guide. The success of ORMTechies in removing a damaging review demonstrates their expertise in review management. Best practices are provided for responding to and leveraging reviews to drive positive change. Best practices are provided for leveraging reviews to drive positive change.   </p><p>A positive online presence is dependent on the services, such as comprehensive monitoring and professional response crafting. ORMTechies commitment to effective online reputation management is reaffirmed in the guide's meta tags and descriptions to optimize visibility and accessibility. </p>"
+           }
+         />
+       </section>
+       <section className="sectionPadding bg_gray">
+         <ImageInfoWrap
+           title={"Best ways to maintain the positive reviews on Glassdoor "}
+           imageUrl={"/images/woman-leaving-good-review-online-product.png"}
+           showPrimeBtn={true}
+           primeBtnName={"get a free quote"}
+           borderBtnName={"faq"}
+           darkButton="dark_btn"
+           description={
+             "<ul><li><b>Employee feedback is encouraged: </b> Encourage your employees to leave reviews on Glassdoor. A mix of positive and constructive feedback provides a more balanced view of your company and can help to dilute the impact of any negative reviews. <li><b>Promoting a positive work environment: </b>Create a positive workplace culture where employees feel valued and heard by investing in creating a positive workplace culture. Your company's reputation can be enhanced by happy employees leaving positive reviews. <li><b>Transparency is important: </b>Transparency in your organization's activities and decision-making procedures fosters confidence among your workforces. Employees are more likely to share positive moments online if they feel informed and involved. <li><b>Promptly address issues: </b>The issues should be addressed promptly and publicly when negative reviews arise. A well-crafted response that acknowledges the problem and outlines steps to resolve it can mitigate the impact of the review.</ul>"
            }
          />
        </section>
@@ -41,7 +68,7 @@ const Glassdoor = () => {
            borderBtnName={"faq"}
            darkButton="dark_btn"
            description={
-             "<p>Negative feedback on Glassdoor poses challenges for organizations, affecting their image and capacity to recruit top talent. The way organizations respond and improve can change if they see these critiques as learning opportunities. Negative feedback can be used by businesses to pinpoint areas for improvement and show concern for staff.   </p><p> A proactive and strategic approach is needed to address negative feedback on Glassdoor. Companies should prioritize transparency and authenticity in their responses, acknowledging the feedback and offering constructive solutions where applicable. Companies should also prioritize transparency and authenticity in their responses. This boosted the company's credibility and trustworthiness and showed a commitment to improvement. </p>"
+             "<p>Negative feedback on Glassdoor poses challenges for organizations, affecting their image and capacity to recruit top talent. The way organizations respond and improve can change if they see these critiques as learning opportunities. Negative feedback can be used by businesses to pinpoint areas for improvement and show concern for staff.   </p><p> A proactive and strategic approach is needed to address negative feedback on Glassdoor. Companies should prioritize transparency and authenticity in their responses, acknowledging the feedback and offering constructive solutions where applicable. Companies should also prioritize transparency and authenticity in their responses. This boosted the company's credibility and trustworthiness and showed a commitment to improvement. </p><p>We help businesses navigate and manage their online reputation, including handling Glassdoor negative reviews effectively. They specialize in helping businesses navigate and manage their online reputation. Our strategies focus on proactive monitoring, response crafting, and fostering a positive workplace culture to mitigate the impact of negative reviews and strengthen employer branding. </p>"
            }
          />
        </section>
@@ -54,7 +81,21 @@ const Glassdoor = () => {
            borderBtnName={"faq"}
            darkButton="dark_btn"
            description={
-             "<p>Managing and removing Glassdoor feedback is essential for preserving a stellar online profile and securing top talent. It is important to handle negative reviews on Glassdoor with care and expertise because they can have a significant impact on a company's image. We offer expert services to assist businesses in navigating and managing their online reputation, including the removal of inappropriate or harmful reviews.  </p><p>Eliminating Glassdoor critiques requires precise preparation. Watching reviews in real-time helps ORMTechies spot any that don't conform to Glassdoor's guidelines or contain erroneous info. After assessing each review thoroughly, we gather evidence and prepare a thorough document to support the removal request. We handle the entire process with professionalism and transparency, ensuring that our client's online presence remains positive and credible. </p>"
+             "<p>Managing and removing Glassdoor feedback is essential for preserving a stellar online profile and securing top talent. It is important to handle negative reviews on Glassdoor with care and expertise because they can have a significant impact on a company's image. We offer expert services to assist businesses in navigating and managing their online reputation, including the removal of inappropriate or harmful reviews.  </p><p>Eliminating Glassdoor critiques requires precise preparation. Watching reviews in real-time helps ORMTechies spot any that don't conform to Glassdoor's guidelines or contain erroneous info. After assessing each review thoroughly, we gather evidence and prepare a thorough document to support the removal request. We handle the entire process with professionalism and transparency, ensuring that our client's online presence remains positive and credible. </p><p>We are committed to helping businesses thrive by managing their online reputation effectively. We are committed to helping businesses thrive by managing their online reputation effectively. Our services include proactive monitoring, strategic response crafting, and professional assistance in review removal, ensuring that our clients' reputations are strong and resilient in the digital age.  </p>"
+           }
+         />
+       </section>
+       <section className="sectionPadding bg_white">
+         <ImageInfoWrap
+           title={"How ORMTechies Can Help "}
+           imageUrl={"/images/feedback-customer-review-rating-concept-people-give-review-rating-feedback-customer-choice.png"}
+           showPrimeBtn={true}
+           isReverse={true}
+           primeBtnName={"get a free quote"}
+           borderBtnName={"faq"}
+           darkButton="dark_btn"
+           description={
+             "<p>ORMTechies, specializing in reputation management, offers a variety of solutions to boost your Glassdoor profile. <ul><li><b>Monitoring and reporting are reviewed:</b> We pay close attention to your Glassdoor profile, delivering regular updates on review fads, prevailing opinions, and overall evaluations. Our insights will enable you to make informed decisions and address any emerging issues promptly. <li><b>Response crafting for professionals: </b>It takes finesse to craft responses to reviews. Our team ensures that your replies are professional, compassionate, and constructive. We take positive and negative reviews and turn them into opportunities for growth. <li><b>The review removal assistance:</b>We value openness, but there are instances where we violate guidelines or provide incorrect information. Glassdoor's guidelines help you spot inappropriate content and, if necessary, ask for its removal. <li><b>The workplace improvement strategies: </b>Reviews offer valuable insights. We help you discover patterns, pinpoint issues, and make positive changes. Whether it's improving work-life balance, communication, or employee recognition, we guide you toward continuous improvement.  </ul></p><p>Our mission at ORMTechies is to transform feedback into useful suggestions that help you improve and boost your organization's image. We can help you improve your overall Glassdoor ratings if you are struggling with negative reviews. Our comprehensive approach ensures that your company shines on Glassdoor. </p>"
            }
          />
        </section>
