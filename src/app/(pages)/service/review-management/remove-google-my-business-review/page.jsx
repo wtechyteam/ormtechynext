727fbf6a6a07397314cb1d2d3689c14434e89ef9
WTechy <wtechy@gmail.com>
2024-08-02 11:35:01 +0530

    google-my business

--- src/app/(pages)/service/review-management/remove-google-my-business-review/page.jsx
+++ src/app/(pages)/service/review-management/remove-google-my-business-review/page.jsx
@@ -15,46 +15,74 @@ const GoogleReviewsInner = () => {
         <meta name="robots" content="index, follow" />
       </head>
       <InnerTopBanner
-        title="Expert Removal Of Google Search Results "
-        info="Unwanted Google results can turn innocent searches into reputation disasters. "
+        title="Review Rewind: Undoing Unwanted Google Feedback"
+        info="Understanding how to remove bad reviews from Google My Business and maintaining a positive online presence is essential for attracting customers and building trust. Negative feedback on search engines like Google can have a significant impact on a company's image, resulting in missed chances and money. "
       />
       <section className="sectionPadding bg_gray">
         <ImageInfoWrap
-          title={"Google’s long memory causes lasting damage"}
+          title={"Is Your Business Leveraging the Power of Google Reviews? "}
           imageUrl={"/images/tiny-people-examining-operating-system-error-warning-web-page-isolated-flat-illustration.png"}
           showPrimeBtn={true}
           primeBtnName={"get a free quote"}
           borderBtnName={"faq"}
           darkButton="dark_btn"
           description={
-            "<p>“The internet never forgets” wouldn’t cause so much misery if it wasn’t for Google being so good at reminding everyone.</p><p>Google’s stated mission is to “to organize the world’s information and make it universally accessible and useful”. What it hasn’t yet reconciled is how unfair and damaging that can be when the “information” being returned starts souring new relationships. Ruining business opportunities. Pausing careers. Stopping the flow of new business.</p><p>Whether you’re a business or an individual, unsavory results in a Google search for your name are an instant poison to your reputation that needs to be dealt with as soon as possible.</p>"
+            "<p>Online reviews hold immense power, particularly Google Reviews, particularly when it comes to online reviews.  Consumers consult online reviews before making a purchase decision, according to studies. Positive feedback can have a significant impact on your customer base, whereas negative feedback can be detrimental.  The importance of managing your online reputation is emphasized by the fact that just one bad review can cause businesses to lose up to 22% of potential customers. </p><p>Google Reviews impact goes beyond influencing consumer behavior, it goes beyond influencing consumer behavior.  Search engine positions also depend on these evaluations. Businesses with higher average ratings and larger numbers of positive reviews are more likely to appear at the top of search results. Your business will be more easily discoverable by potential customers if you have greater online visibility.  Positive reviews build trust with users and encourage them to choose their business over competitors.</p><p> Businesses can use the power of Google Reviews to their advantage by understanding them.  In the next section, we will explore how ORM Techies can help you build and manage a positive online reputation through effective Google Review strategies.</p> "
           }
         />
       </section>
       <section className="sectionPadding bg_white">
         <ImageInfoWrap
-          title={"You can defend your reputation with ORMTechies "}
-          imageUrl={"/images/5002461.jpg"}
+          title={"Decoding Negative Feedback"}
+          imageUrl={"/images/social_media_dislike.jpg"}
           showPrimeBtn={true}
           isReverse={true}
           primeBtnName={"get a free quote"}
           borderBtnName={"faq"}
           darkButton="dark_btn"
           description={
-            "<p> Google is a complex beast, but sometimes the solutions are simple – you just need to know how. Since every search result is different, the first step is always to reach out and get a free consultation. At worst, you’ll at least find out the situation and possible solutions, which may not be immediately obvious to you otherwise.</p><p>You’ll be contacting a team with enormous experience and resources in this field, including our own custom-built technology platform. Whether it is complete and permanent removal, or suppressing the content away from Page 1 of Google, you can rely on ORMTechies to free you from the burden of an unwanted Google search result.</p>"
+            "<p> Even companies with top-notch offerings can be hit hard by negative feedback. These reviews can arise from a variety of factors, including genuine misunderstandings between the customer and business, malicious intent from competitors or trolls, or even lingering dissatisfaction from past issues. Negative reviews have the power to damage a business's reputation and discourage potential customers. </p><p>Negative feedback needs to be dealt with promptly and professionally. However, a new challenge arises when the review appears unfair, inaccurate, or even fabricated. Navigating the online reputation landscape becomes incredibly challenging. ORM Techies comes in to help with online reputation management. We understand the delicate dance of responding to negative reviews, and we offer guidance on how to address legitimate concerns while also mitigating the impact of unjust criticism. </p>"
           }
         />
       </section>
       <section className="sectionPadding bg_gray">
         <ImageInfoWrap
-          title={"The impact of negative search results"}
+          title={"Reputation Recovery: How to Rebuild after Crisis "}
+          imageUrl={"/images/5002461.jpg"}
+          showPrimeBtn={true}
+        
+          primeBtnName={"get a free quote"}
+          borderBtnName={"faq"}
+          darkButton="dark_btn"
+          description={
+            "<p> Building a positive reputation on Google My Business takes a proactive approach to building a positive reputation.  You can make it easy for satisfied customers to leave positive reviews by providing a link to your Google My Business profile. Positive experiences are more likely to translate into positive reviews if, they are positive. Make sure your team prioritizes client contentment and handles any issues swiftly. Set up alerts to stay updated on new reviews by setting up new alerts.  </p><p>This will allow you to address any concerns promptly. Businesses facing many negative reviews or struggling to manage their online reputation can benefit from professional online reputation management services. Strategies for responding to negative feedback and minimizing their impact can be found in these solutions. Businesses can build a positive online reputation on Google My Business by implementing these strategies consistently, leading to increased trust and visibility among potential customers. </p>"
+          }
+        />
+      </section>
+      <section className="sectionPadding bg_white">
+        <ImageInfoWrap
+          title={"The ORM Techies Advantage"}
+          imageUrl={"/images/4445857.jpg"}
+          showPrimeBtn={true}
+          isReverse={true}
+          primeBtnName={"get a free quote"}
+          borderBtnName={"faq"}
+          darkButton="dark_btn"
+          description={
+            "<p> Our dedication to client satisfaction and results-driven approach are what distinguishes ORM Techies. We place a high priority on transparency, communication, and integrity in everything we do, ensuring that our clients feel supported and empowered throughout their reputation management journey. </p><p>ORM Techies can help restore your rating reputation with Google My Business by helping restore your rating reputation. <ol><li>The root cause of the negative ratings will be understood by analyzing your current Google My Business profile and reviews.<li>For legitimate concerns, we can help you develop a professional and empathetic response plan. This could involve offering solutions, apologizing for mistakes, or clarifying misunderstandings. <li>We will use strategies to encourage satisfied customers to leave positive reviews. Sending review requests after a positive encounter or highlighting the <b>Write a Review</b> button on your website and social media could be ways to accomplish this.<li>We'll keep an eye on your GMB profile, respond to any new feedback, and keep an eye on your overall score. We will provide regular reports on your progress and adjust our strategy as needed. We will adjust our strategy as needed.<li>We will guide you through the process of flagging fake or irrelevant reviews for the review team in cases of suspected fake or irrelevant reviews. We can help you write a clear and concise explanation for Google on why the review should be removed.    </ol>Implementing a comprehensive strategy that addresses both positive and negative feedback, ORM Techies can assist you in rebuilding your reputation and enhancing your Google My Business score. </p>"
+          }
+        />
+      </section>
+      <section className="sectionPadding bg_gray">
+        <ImageInfoWrap
+          title={"Google Reviews Unleashed: Boost Your Brand "}
           imageUrl={"/images/complain-concept-with-one-star-review.png"}
           showPrimeBtn={true}
           primeBtnName={"get a free quote"}
           borderBtnName={"faq"}
           darkButton="dark_btn"
           description={
-            "<h4>For Businesses</h4><p>When negative results pop up beside your company name, it can stop people in their tracks before you’ve had a chance to interact. Online, your reputation is everything: a negative review, unpleasant article, or aggressive discussions about your business could all have a massive impact on performance.</p><h4>For Individuals</h4><p>Like any brand, a personal brand can be enormously tarnished by unwanted Google search results. Prospective employers, clients, or investors are all liable to look you up on Google. This goes double for jobs built on reputation: doctors, lawyers, and many other professionals rely on us to defend their online presence.</p><p>Often it’s not even something you’ve done: simple association with a negative event or disreputable company can cause long lasting damage.</p><p>Whatever your situation, unfortunately these are problems that rarely go away by themselves. Dealing with them immediately is the only way to limit the cost and damage. </p>"
+            "<p>Reputation management can include negative removing google reviews, but ORM Techies goes beyond that. Our Google review management solutions are aimed at proactively preserving your online reputation.  A positive review culture is a space where satisfied customers are encouraged to share their experiences and amplify your business's positive image online.</p><h4>This is how we do it:</h4><p><ul><li>New reviews left on your Google My Business profile are monitored by us. We can address any concerns raised by customers, demonstrating your commitment to transparency and responsiveness. <li>We encourage positive reviews, not just wait for them to happen. We help you develop strategies to encourage satisfied customers to leave positive feedback. We can help you develop these strategies. <li>We help you craft professional and appropriate responses when feedback hits, whether it's positive or negative. This will foster trust with potential customers by demonstrating how you value feedback and prioritize customer satisfaction. </ul>Google review management empowers your business to maintain a positive online reputation by implementing these proactive measures. Increased trust from potential customers who see your commitment to exceptional service through the lens of genuine customer experiences is achieved. </p><p>A proactive approach is required for building a positive reputation on Google My Business. For businesses facing a significant number of negative reviews, professional ORM services can offer strategies for reputation management. ORM Techies goes beyond just removing negative reviews to provide comprehensive review management services.  Our focus is on creating a positive review culture where satisfied customers are encouraged to share their experiences and amplify your online presence. </p>"
           }
         />
       </section>
