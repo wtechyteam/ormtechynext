53ad3c84a3e92e0ec04641d7571d53f439cacb80
WTechy <wtechy@gmail.com>
2024-08-05 11:46:04 +0530

    knowledge panel

--- src/app/(pages)/service/reputation-repair/knowledge-panel/page.jsx
+++ src/app/(pages)/service/reputation-repair/knowledge-panel/page.jsx
@@ -25,7 +25,7 @@ const LegalReputationManagement = () => {
           borderBtnName={"faq"}
           darkButton="dark_btn"
           description={
-            "<p>In the digital age, being seen and being shown accurately are crucial for any brand to succeed. Knowledge Panels are prominently displayed on search engine result pages. They offer a short and reliable overview of your company. We at ORM Techies are experts at managing and improving these panels. We make sure they truthfully and favorably convey your company's essence.  </p><p>With our experience, we can improve your web presence, take charge of the story, and draw attention to important details. Our services are for companies of any size. They are meant to boost your reputation and exposure. </p><p>They aim to ensure that everyone who searches for your brand is left with a good impression. We improve how search engines understand your content. We do this by using structured data and schema markup. Our strategy includes proactive upgrades. We also do ongoing monitoring. It's to make sure your Knowledge Panel is up to date and effective. You can control the story about your business. You can also improve your online presence and leave a lasting impression on potential clients. You can do this using ORM Techies. You may rely on us to handle your Knowledge Panel with accuracy and skill. </p>"
+            "<p>In the digital age, being seen and being shown accurately are crucial for any brand to succeed. Knowledge Panels are prominently displayed on search engine result pages. They offer a short and reliable overview of your company. We at ORM Techies are experts at managing and improving these panels. We make sure they truthfully and favorably convey your company's essence.  </p><p>With our experience, we can improve your web presence, take charge of the story, and draw attention to important details. Our services are for companies of any size. They are meant to boost your reputation and exposure. </p><p>They aim to ensure that everyone who searches for your brand is left with a good impression. We improve how search engines understand your content. We do this by using structured data and schema markup.</p><p> Our strategy includes proactive upgrades. We also do ongoing monitoring. It's to make sure your Knowledge Panel is up to date and effective. You can control the story about your business. You can also improve your online presence and leave a lasting impression on potential clients. You can do this using ORM Techies. You may rely on us to handle your Knowledge Panel with accuracy and skill. </p>"
           }
         />
       </section>
@@ -47,14 +47,56 @@ const LegalReputationManagement = () => {
       </section>
       <section className="sectionPadding bg_gray">
         <ImageInfoWrap
-          title={"Unmatched Benefits of Choosing ORM Techies  "}
+          title={"Strategic Approach to Knowledge Panel Optimization  "}
           imageUrl={"/images/man-woman-touching-each-other-when-work-is-done.png"}
           showPrimeBtn={true}
           primeBtnName={"get a free quote"}
           borderBtnName={"faq"}
           darkButton="dark_btn"
           description={
+            "<p>Knowledge panel optimization is something we at ORM Techies do methodically and strategically. We start by auditing your internet presence. We do this to find holes or areas for improvement. Next, we ensure the information we have about your brand is accurate and thorough. It must also fit your company's beliefs and aims.  </p><p>This includes adding professional photos and logos. It also involves checking contact info and updating business descriptions. To build trust, we also work hard to find and show positive comments and testimonies. To help search engines understand your site, we use schema markup and structured data.  </p><p>We must keep the Knowledge Panel up to date. Our staff constantly monitors it and makes the necessary updates. We are proficient. We can ensure that your Knowledge Panel represents your company well. It will attract the right readers. </p>"
+          }
+        />
+      </section>
+      <section className="sectionPadding bg_white">
+        <ImageInfoWrap
+          title={"Unmatched Benefits of Choosing ORM Techies  "}
+          imageUrl={"/images/2893647.jpg"}
+          showPrimeBtn={true}
+          isReverse={true}
+          primeBtnName={"get a free quote"}
+          borderBtnName={"faq"}
+          darkButton="dark_btn"
+          description={
             "<p>At ORM Techies, we have a team of seasoned experts. They have a lot of knowledge in online reputation management and digital marketing. We are aware of the subtleties of Knowledge Panels and the most recent advancements in SEO. We acknowledge that every firm has distinct needs. </p><p>Our knowledge panel management strategy is very individualized. We work directly with you. We do this to understand your target market, industry, and business goals. We make sure your Knowledge Panel fully and accurately represents your company. This means adding great photos and logos. It also means showing positive reviews and testimonials. And it means revising business descriptions. </p><p> Lastly, it means confirming contact information. We help search engines understand and show your content. We do this using structured data and schema markup. This boosts the visibility of your Knowledge Panel on search result pages. It also improves your SEO. This results in more organic traffic to your website. </p>"
+          }
+        />
+      </section>
+      <section className="sectionPadding bg_gray">
+        <ImageInfoWrap
+          title={"Transforming Client Success through Knowledge Panels "}
+          imageUrl={"/images/NA_SEP._25-removebg-preview.png"}
+          showPrimeBtn={true}
+          
+          primeBtnName={"get a free quote"}
+          borderBtnName={"faq"}
+          darkButton="dark_btn"
+          description={
+            "<p>Our companies used our Knowledge Panel optimization services. They saw big gains in their web visibility and customer interaction. Clients gain credibility and trust from their audience. They do this by providing accurate and appealing information. </p><p>More website traffic and better conversion rates are the outcomes. They come from higher visibility in search results for their brands. Also, positive evaluations and testimonials help build a base of loyal clients. They portray the company favorably. </p><p>The digital market is always changing. Our clients profit from our ongoing support and updates. These things ensure that their knowledge panels stay effective and relevant. Businesses may focus on their main skills. We will handle their online reputation when they work with ORM Techies.</p>"
+          }
+        />
+      </section>
+      <section className="sectionPadding bg_white">
+        <ImageInfoWrap
+          title={"Unwavering Commitment to Excellence "}
+          imageUrl={"/images/268150-P5HM3K-446-removebg-preview.png"}
+          showPrimeBtn={true}
+          isReverse={true}
+          primeBtnName={"get a free quote"}
+          borderBtnName={"faq"}
+          darkButton="dark_btn"
+          description={
+            "<p>At ORM Techies, we strive for perfection in everything we do. We are committed to surpassing our clients' expectations. We offer excellent Knowledge Panel management services.  </p><p>To get the best results, our staff stays up to date on the newest trends in SEO and digital marketing. We take great pride in our close attention to detail. We ensure that each item we select and update is reliable and important.  </p><p>We use a client-centric approach. We customize our tactics to fit your unique business requirements. We offer individualized, outcome-driven services. You may be sure that ORM Techies has the expertise to handle the internet reputation of your brand. </p>"
           }
         />
       </section>
