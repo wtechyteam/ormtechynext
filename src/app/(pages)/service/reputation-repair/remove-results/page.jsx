b9f3cc88435b2d9e18f24e444eb40407aa0b60b9
WTechy <wtechy@gmail.com>
2024-08-05 16:24:17 +0530

    all content upload complete

--- src/app/(pages)/service/reputation-repair/remove-results/page.jsx
+++ src/app/(pages)/service/reputation-repair/remove-results/page.jsx
@@ -25,14 +25,14 @@ const LegalReputationManagement = () => {
           borderBtnName={"faq"}
           darkButton="dark_btn"
           description={
-            "<p>Your online reputation is crucial. It's a part of your personal or professional identity in the digital age. You can seriously harm your reputation. And, you can miss out on chances from just one bad search result.   </p><p>ORM Techies can be of assistance in this situation. You or your company's best image is online.  We are experts at managing and improving your online reputation. We customize our tactics to fit your needs.  </p>"
+            "<p>Your online reputation is crucial. It's a part of your personal or professional identity in the digital age. You can seriously harm your reputation. And, you can miss out on chances from just one bad search result.   </p><p>ORM Techies can be of assistance in this situation. You or your company's best image is online.  We are experts at managing and improving your online reputation. We customize our tactics to fit your needs.  </p><p>We recognize that every client is different. We ensure your internet reputation is influential and positive. This continues over time with our ongoing reporting and monitoring. Make a good impression online. Stay that way by working with ORM Techies to control your image. Allow us to assist you in reinventing your online presence. </p>"
           }
         />
       </section>
       <section className="sectionPadding bg_white">
         <ImageInfoWrap
           title={
-            "Advanced Tactics for Outstanding Reviews  "
+            "The Power of a Good Reputation  "
           }
           imageUrl={"/images/business-direction-abstract-concept-illustration.png"}
           showPrimeBtn={true}
@@ -41,20 +41,62 @@ const LegalReputationManagement = () => {
           borderBtnName={"faq"}
           darkButton="dark_btn"
           description={
-            "<p>Choosing ORM Techies for your online reputation management could greatly enhance your brand. They offer many benefits. We guarantee customized plans. They will meet your goals. Our staff has lots of experience in many sectors. </p><p>We provide all-inclusive solutions. They include content creation, SEO, and proactive review management. They improve your online reputation. Our tailored approaches address specific problems and goals. They guarantee individualized solutions that provide results.  </p><p>We actively keep an eye on reviews and reply to them to sustain a constantly favorable web presence. Our data-driven techniques provide quantifiable gains, boosting client loyalty and trust.Choosing ORM Techies means getting a partner. They are committed to protecting and improving your online reputation. This will boost visibility, client interaction, and business expansion. To stand out in today's crowded market, let us help you make a strong and nice website. </p>"
+            "<p>Your reputation on the internet has tremendous power. It shapes how possible clients, consumers, and partners view you or your company. You must create a positive online presence. Users never scroll past the first page of search results. You could lose revenue.  </p><p>You could damage your reputation with just one bad review or unfavorable article. Building trust with others is through your online reputation. It encourages them to interact with you or your brand. It also fosters confidence. It directly impacts sales, partnerships, and opportunities. So, it is essential to your success. A damaged reputation might result in diminished sales. </p><p>It would also mean fewer devoted customers and a tarnished reputation. Considering this, a good online reputation is crucial today. It is also advantageous. To succeed in a cutthroat market, ensure your online persona shows your values and assets. </p>"
           }
         />
       </section>
       <section className="sectionPadding bg_gray">
         <ImageInfoWrap
-          title={"Commitment to Excellence in Reputation "}
+          title={"Tailored Strategies for Online Success "}
           imageUrl={"/images/businesswoman-with-coins-cup-prize-with-gears.png"}
           showPrimeBtn={true}
           primeBtnName={"get a free quote"}
           borderBtnName={"faq"}
           darkButton="dark_btn"
           description={
-            "<p>At ORM Techies, we strive for perfection in everything we do. Our dedication is to making great, captivating material. It will exactly represent your company and resonate with your customers. We improve our techniques to best help our clients. We base the improvements on recent developments and user input. Our top goal is seeing our clients succeed.  </p><p>Our goal is to fully understand your demands. We will do this through close collaboration and by providing exceptional, customized solutions. Sincerity and openness characterize our business practices. We give you honest evaluations and outcomes and keep you updated at every stage. </p>"
+            "<p>We use the strategy at ORM Techies for online reputation management. It is thorough and tailored to your needs. We start by evaluating your internet presence. We look for harmful information and possible risks.  </p><p>Afterward, we use digital public relations, SEO, and content development. Our team creates a plan to deal with these problems. We make great material. It will outweigh bad search results and show your best qualities. Our SEO specialists also put in a lot of effort. They optimize this material so that it appears higher in search results. We also interact with internet groups and forums.</p><p>We do this to create a good story about your company. We reach out to reputable websites and media sources. This is to publish positive articles and press releases about you or your company. It is part of our digital public relations activities. This increases credibility and trust. It also helps remove bad search results. We guarantee a full strategy to keep and improve your internet reputation. We do this by combining these strategies. </p>"
+          }
+        />
+      </section>
+     
+      <section className="sectionPadding bg_white">
+        <ImageInfoWrap
+          title={"Why Choose ORM Techies?  "}
+          imageUrl={"/images/cf102b13-2a73-44f3-9b10-af2d975c9721.jpg"}
+          showPrimeBtn={true}
+          isReverse={true}
+          primeBtnName={"get a free quote"}
+          borderBtnName={"faq"}
+          darkButton="dark_btn"
+          description={
+            "<p>Our services provide significant benefits to our clients. They notice an immediate improvement in their internet reputation. They see it after working with ORM Techies. Good content appears at the top of search results.  </p><p>It pushes down negative results. This brings in new business. It also helps restore lost confidence. As businesses' online reputation grows, they see a clear boost in sales. They also see more client involvement. Customers are more likely to choose and trust a company with a strong internet presence. </p><p> A good online presence is captivating. It helps people build their personal brands and find new job prospects. Our clients gain from our ongoing oversight and assistance as well. We make sure that your internet reputation is kept over time. We don't just solve the issue and move on. The digital landscape is always changing. A strong online presence depends on this support. </p>"
+          }
+        />
+      </section>
+      <section className="sectionPadding bg_gray">
+        <ImageInfoWrap
+          title={"How Our Clients Benefit "}
+          imageUrl={"/images/happy-woman-getting-promotion-financial-freedom-celebrating-success-work.png"}
+          showPrimeBtn={true}
+          primeBtnName={"get a free quote"}
+          borderBtnName={"faq"}
+          darkButton="dark_btn"
+          description={
+            "<p>Our services provide significant benefits to our clients. They see an instant boost in their online reputation. It happens after working with ORM Techies. Positive material is at the top of the search results. It pushes down negative results. </p><p>This brings new chances. It also helps win back lost trust. When a business's online reputation grows, sales and customer engagement both go up. A company with a good internet reputation is more likely to earn the trust of potential customers. They also have a higher chance of earning their business. A tidy, striking website improves personal branding. It leads to new job prospects for people.  </p><p>Our clients gain from our ongoing oversight and assistance as well. We make sure to keep up your internet reputation over time. We don't just fix the issue and move on. The digital landscape is always changing. A strong online presence depends on this support. Our clients gain from our ongoing oversight and assistance as well. We make sure to keep up your internet reputation over time. We don't just fix the issue and move on. The digital landscape is always changing. A strong online presence depends on this support. </p>"
+          }
+        />
+      </section>
+      <section className="sectionPadding bg_white">
+        <ImageInfoWrap
+          title={"Our Commitment to Excellence"}
+          imageUrl={"/images/8638979-removebg-preview.png"}
+          showPrimeBtn={true}
+          isReverse={true}
+          primeBtnName={"get a free quote"}
+          borderBtnName={"faq"}
+          darkButton="dark_btn"
+          description={
+            "<p>Our dedication to perfection is unshakable at ORM Techies. We are committed to giving our clients the best service. We also make sure their internet reputation is in good hands. Our team of pros is committed to staying at the industry's forefront. We often update our tools and methods.  </p><p>We do this to reflect the newest online reputation trends and tech. We know that every client is different and has different demands. As a result, we provide customized solutions that are made to match your unique needs. We aim to offer outcomes that will improve your internet presence.  </p><p>They will do so for a long time and go beyond your expectations. Our principles are centered around transparency, ethics, and dedication. We aim to build a partnership based on respect and trust. We ensure that our clients are fully informed and involved at every stage. We strive to give our clients only the best. We are dedicated to perfection. </p>"
           }
         />
       </section>
