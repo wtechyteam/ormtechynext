53bbb9a8a56aba594018a1ff4d9d214a699a20ee
WTechy <wtechy@gmail.com>
2024-09-11 14:56:31 +0530

    pricing Form

--- src/app/(pages)/pricing/quoteForm/page.jsx
+++ src/app/(pages)/pricing/quoteForm/page.jsx
@@ -12,8 +12,8 @@ const QuoteForm = () => {
             
                     <h3 className='title-lg dark_font text-center fw-bold margin_bottom_40'>Fill Up Details To get a Quote</h3>
                     <HubspotForm 
-                portalId='46372694'
-                formId='5057504d-4b19-41c8-88ce-a34caae4ffd0'
+                portalId='47335383'
+                formId='5d75cf09-1c90-48ef-b913-288c14d1791d'
                 onSubmit={() => console.log('Submit!')}
                 onReady={(form) => console.log('Form ready!')}
                 loading={<div>Loading...</div>}
